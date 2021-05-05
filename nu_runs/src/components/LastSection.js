ec736e56de21510fdf0433152aa79ea98a8a3a87
Kawal <knsbatra@gmail.com>
2021-05-05 21:50:34 +0530

    LastSection which contains the getting started and blog subscripion button

---
+++ nu_runs/src/components/LastSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../css/lastsection.css";
+
+const LastSection = ()=>{
+    return <>
+    <Jumbotron className="signupjumbo">
+        <h1>Sign Up For Free Today</h1>
+<Link to="/get-started"> <button>Get Started</button></Link>       
+    </Jumbotron>
+    </>;
+
+}
+export default LastSection;
